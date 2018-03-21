34e87cb9ac5f9d65cc2532b6c5c0fdf09b4b4f7b
Dylan QUENTIN <dylan.quentin@etudiant.univ-lille1.fr>
2018-03-21 16:45:23 +0100

    voyageresource

---
+++ server/src/main/java/fr/iutinfo/skeleton/api/VoyageResource.java
@@ -0,0 +1,154 @@
+package fr.iutinfo.skeleton.api;
+
+import static fr.iutinfo.skeleton.api.BDDFactory.getDbi;
+import static fr.iutinfo.skeleton.api.BDDFactory.tableExist;
+
+<<<<<<< HEAD
+import java.net.URI;
+=======
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+>>>>>>> 970bc581bb3765770b54287930ea795bc5271752
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+<<<<<<< HEAD
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+=======
+import javax.ws.rs.*;
+>>>>>>> 970bc581bb3765770b54287930ea795bc5271752
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+
+<<<<<<< HEAD
+@Path("/voyage")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+public class VoyageResource {
+    final static Logger logger = LoggerFactory.getLogger(VoyageResource.class);
+    private static VoyageDAO dao = getDbi().open(VoyageDAO.class);
+	private static Map<Integer, Voyage> voyages = new HashMap<>();
+	@Context
+	public UriInfo uriInfo;
+    public VoyageResource() throws SQLException {
+        if (!tableExist("voyage")) {
+            logger.debug("Create table voyage");
+            dao.createVoyageTable();
+
+            dao.insert(new Voyage(1, 1 , "Petit weekend à Paris","Paris"));
+
+        }
+    }
+
+    @POST
+    public Response createVoyage(Voyage voyage) {      
+    	   if ( voyages.containsKey(voyage.getId()) ) {
+               return Response.status(Response.Status.CONFLICT).build();
+           }
+           else {
+               voyages.put(voyage.getId(), voyage);
+
+               // On renvoie 201 et l'instance de la ressource dans le Header HTTP 'Location'
+               String tp = "" + voyage.getId();
+               URI instanceURI = uriInfo.getAbsolutePathBuilder().path(tp).build();
+               return Response.created(instanceURI).build();
+           }
+    }
+	@GET
+	public List<Voyage> getPizzas() {
+		return new ArrayList<Voyage>(voyages.values());
+	}
+
+  /*  @GET
+    @Path("/{ville}")
+    public UserDto getVille(@PathParam("ville") String name) {
+        Voyage voyage = dao.findByName(name);
+        if (user == null) {
+            throw new WebApplicationException(404);
+        }
+        return user.convertToDto();
+    }
+=======
+import fr.iutinfo.skeleton.common.dto.VoyageDto;
+
+public class VoyageResource {
+	@Path("/voyage")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public class UserResource {
+	    final Logger logger = LoggerFactory.getLogger(VoyageResource.class);
+	    private VoyageDao dao = getDbi().open(VoyageDao.class);
+
+	    public UserResource() throws SQLException {
+	        if (!tableExist("users")) {
+	            logger.debug("Create table users");
+	            dao.createVoyageTable();
+	            dao.insert(new Voyage(0,0, "Camping à plusieurs proche de la mer", "Dunkerque"));
+	        }
+	    }
+
+	    @POST
+	    public VoyageDto createVoyage(VoyageDto dto,int idUser) {
+	        Voyage voyage = new Voyage();
+	        voyage.initFromDto(dto);
+	        int id = dao.insert(voyage);
+	        dto.setId(id);
+	        dto.setIdUser(idUser);
+	        return dto;
+	    }
+>>>>>>> 970bc581bb3765770b54287930ea795bc5271752
+
+	    @GET
+	    @Path("/{name}")
+	    public VoyageDto getVoyage(@PathParam("name") String name) {
+	        Voyage voyage = dao.findByName(name);
+	        if (voyage == null) {
+	            throw new WebApplicationException(404);
+	        }
+	        return voyage.convertToDto();
+	    }
+
+	    @GET
+	    public List<VoyageDto> getAllVoyages(@QueryParam("q") String query) {
+	        List<Voyage> voyages;
+	        if (query == null) {
+	            voyages = dao.all();
+	        } else {
+	            logger.debug("Search voyages with query: " + query);
+	            voyages = dao.search("%" + query + "%");
+	        }
+	        return voyages.stream().map(Voyage::convertToDto).collect(Collectors.toList());
+	    }
+
+	    @DELETE
+	    @Path("/{id}")
+	    public void deleteVoyage(@PathParam("id") int id) {
+	        dao.delete(id);
+	    }
+	    
+		@PUT
+	    @Path("/{id}")
+	    public Response modifVoyage(@PathParam("id") int id, Voyage voyage) {
+	        // Si l'utilisateur est inconnu, on renvoie 404
+	        if (id != voyage.getId()) {
+		    throw new NotFoundException();
+	        }
+	        else {
+	        	dao.update(id, voyage);
+		    return Response.status(Response.Status.NO_CONTENT).build();
+	        }
+	    }
+	}
+}
