3c6d7c18a52619ec5e9457c00afd217a4d7e16b9
Kaelig CATTEAU <kaelig.catteau@etudiant.univ-lille1.fr>
2018-03-21 14:04:58 +0100

    User

--- server/src/main/java/fr/iutinfo/skeleton/api/User.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/User.java
@@ -14,9 +14,15 @@ public class User implements Principal {
     final static Logger logger = LoggerFactory.getLogger(User.class);
     private static User anonymous = new User(-1, "Anonymous", "anonym");
     private String name;
+    private String prenom;
     private String alias;
+    private String tel;
+    private String daten;
     private int id = 0;
     private String email;
+    private String adresse;
+   
+    
     private String password;
     private String passwdHash;
     private String salt;
@@ -32,11 +38,58 @@ public class User implements Principal {
         this.name = name;
         this.alias = alias;
     }
+    
+    public User(String name, String prenom, String alias, String tel, String daten, int id, String email,
+			String adresse) {
+		super();
+		this.name = name;
+		this.prenom = prenom;
+		this.alias = alias;
+		this.tel = tel;
+		this.daten = daten;
+		this.id = id;
+		this.email = email;
+		this.adresse = adresse;
+	}
 
-    public User() {
+	public User() {
     }
 
-    public static User getAnonymousUser() {
+	
+	
+    public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getDaten() {
+		return daten;
+	}
+
+	public void setDaten(String daten) {
+		this.daten = daten;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public static User getAnonymousUser() {
         return anonymous;
     }
 
@@ -96,24 +149,77 @@ public class User implements Principal {
     }
 
     @Override
-    public boolean equals(Object arg) {
-        if (getClass() != arg.getClass())
-            return false;
-        User user = (User) arg;
-        return name.equals(user.name) && alias.equals(user.alias) && email.equals(user.email) && passwdHash.equals(user.getPasswdHash()) && salt.equals((user.getSalt()));
-    }
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (alias == null) {
+			if (other.alias != null)
+				return false;
+		} else if (!alias.equals(other.alias))
+			return false;
+		if (daten == null) {
+			if (other.daten != null)
+				return false;
+		} else if (!daten.equals(other.daten))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (passwdHash == null) {
+			if (other.passwdHash != null)
+				return false;
+		} else if (!passwdHash.equals(other.passwdHash))
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		if (salt == null) {
+			if (other.salt != null)
+				return false;
+		} else if (!salt.equals(other.salt))
+			return false;
+		if (tel == null) {
+			if (other.tel != null)
+				return false;
+		} else if (!tel.equals(other.tel))
+			return false;
+		return true;
+	}
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((alias == null) ? 0 : alias.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((passwdHash == null) ? 0 : passwdHash.hashCode());
-        result = prime * result + ((salt == null) ? 0 : salt.hashCode());
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((alias == null) ? 0 : alias.hashCode());
+		result = prime * result + ((daten == null) ? 0 : daten.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((passwdHash == null) ? 0 : passwdHash.hashCode());
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		result = prime * result + ((salt == null) ? 0 : salt.hashCode());
+		result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+		return result;
+	}
     
     @Override
     public String toString() {
