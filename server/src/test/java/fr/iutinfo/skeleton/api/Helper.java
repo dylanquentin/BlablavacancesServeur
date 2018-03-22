08c5325c871021e098d6f9df79e5bf34d498a41d
Kaelig CATTEAU <kaelig.catteau@etudiant.univ-lille1.fr>
2018-03-22 10:08:16 +0100

    reset

--- server/src/test/java/fr/iutinfo/skeleton/api/Helper.java
+++ server/src/test/java/fr/iutinfo/skeleton/api/Helper.java
@@ -1,123 +1,77 @@
 package fr.iutinfo.skeleton.api;
 
-import java.util.Calendar;
-import java.util.List;
-
-import javax.ws.rs.core.GenericType;
-
+import fr.iutinfo.skeleton.common.dto.UserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.iutinfo.skeleton.common.dto.UserDto;
+import javax.ws.rs.core.GenericType;
+import java.util.List;
 
 public class Helper {
-	private final static Logger logger = LoggerFactory.getLogger(Helper.class);
-	private static final UserDao daoU = BDDFactory.getDbi().open(UserDao.class);
-	private static final VoyageDao daoV = BDDFactory.getDbi().open(VoyageDao.class);
+    private final static Logger logger = LoggerFactory.getLogger(Helper.class);
+    private static final UserDao dao = BDDFactory.getDbi().open(UserDao.class);
+    static GenericType<List<UserDto>> listUserResponseType = new GenericType<List<UserDto>>() {
+    };
 
-	static GenericType<List<UserDto>> listUserResponseType = new GenericType<List<UserDto>>() {
-	};
-	static GenericType<List<UserDto>> listVoyageResponseType = new GenericType<List<UserDto>>() {
-	};
+    public static void initDb() {
+        dao.dropUserTable();
+        dao.createUserTable();
+    }
 
-	public static void initDb() {
-		daoU.dropUserTable();
-		daoU.createUserTable();
-		daoV.dropVoyageTable();
-		daoV.createVoyageTable();
-	}
+    static User createUserWithName(String name) {
+        User user = new User(0, name);
+        return createUser(user);
+    }
 
-	static Voyage createVoyageWithName(String name, String ville) {
-		Voyage voyage = new Voyage(0, 2, name, ville);
-		return createVoyage(voyage);
-	}
+    static User createUserWithAlias(String name, String alias) {
+        User user = new User(0, name, alias);
+        return createUser(user);
+    }
 
-	private static Voyage createVoyage(Voyage voyage) {
-		int id = daoV.insert(voyage);
-		voyage.setId(id);
-		return voyage;
-	}
+    static User createUserWithEmail(String name, String email) {
+        User user = new User(0, name);
+        user.setEmail(email);
+        return createUser(user);
+    }
 
-	private static Voyage createFullVoyage(int id, int idUser, String name, String ville, String description,
-			Calendar depart, Calendar retour, int capacite) {
-		Voyage voyage = new Voyage(id, idUser, name, ville);
-		voyage.setDescription(description);
-		voyage.setDepart(depart);
-		voyage.setRetour(retour);
-		int idV = daoV.insert(voyage);
-		voyage.setId(idV);
-		return voyage;
-	}
+    public static User createUserWithPassword(String name, String passwd, String salt) {
+        User user = new User(0, name);
+        user.setSalt(salt);
+        user.setPassword(passwd);
+        logger.debug("createUserWithPassword Hash : " + user.getPasswdHash());
+        return createUser(user);
+    }
 
-	static void createvl() {
-		createFullVoyage(0, 10, "test", "Lille", "blabla", Calendar.getInstance(), Calendar.getInstance(), 3);
-	}
+    private static User createUser(User user) {
+        int id = dao.insert(user);
+        user.setId(id);
+        return user;
+    }
 
-	static Voyage createvp() {
-		return createFullVoyage(1, 10, "test", "Paris", "blabla", Calendar.getInstance(), Calendar.getInstance(), 3);
-	}
 
-	static Voyage createvn() {
-		return createFullVoyage(2, 10, "test", "Nice", "blabla", Calendar.getInstance(), Calendar.getInstance(), 3);
-	}
+    private static User createFullUSer(String name, String alias, String email, String paswword) {
+        User user = new User(0, name);
+        user.setAlias(alias);
+        user.setEmail(email);
+        user.setPassword(paswword);
+        int id = dao.insert(user);
+        user.setId(id);
+        return user;
+    }
 
-	static Voyage createvb() {
-		return createFullVoyage(3, 10, "test", "Biarritz", "blabla", Calendar.getInstance(), Calendar.getInstance(), 3);
-	}
+    static void createRms() {
+        createFullUSer("Richard Stallman", "RMS", "rms@fsf.org", "gnuPaswword");
+    }
 
-	static User createUserWithName(String name) {
-		User user = new User(0, name);
-		return createUser(user);
-	}
+    static User createRob() {
+        return createFullUSer("Robert Capillo", "rob", "rob@fsf.org", "paswword");
+    }
 
-	static User createUserWithAlias(String name, String alias) {
-		User user = new User(0, name, alias);
-		return createUser(user);
-	}
+    static User createLinus() {
+        return createFullUSer("Linus Torvalds", "linus", "linus@linux.org", "paswword");
+    }
 
-	static User createUserWithEmail(String name, String email) {
-		User user = new User(0, name);
-		user.setEmail(email);
-		return createUser(user);
-	}
-
-	public static User createUserWithPassword(String name, String passwd, String salt) {
-		User user = new User(0, name);
-		user.setSalt(salt);
-		user.setPassword(passwd);
-		logger.debug("createUserWithPassword Hash : " + user.getPasswdHash());
-		return createUser(user);
-	}
-
-	private static User createUser(User user) {
-		int id = daoU.insert(user);
-		user.setId(id);
-		return user;
-	}
-
-	private static User createFullUSer(String name, String alias, String email, String paswword) {
-		User user = new User(0, name);
-		user.setAlias(alias);
-		user.setEmail(email);
-		user.setPassword(paswword);
-		int id = daoU.insert(user);
-		user.setId(id);
-		return user;
-	}
-
-	static void createRms() {
-		createFullUSer("Richard Stallman", "RMS", "rms@fsf.org", "gnuPaswword");
-	}
-
-	static User createRob() {
-		return createFullUSer("Robert Capillo", "rob", "rob@fsf.org", "paswword");
-	}
-
-	static User createLinus() {
-		return createFullUSer("Linus Torvalds", "linus", "linus@linux.org", "paswword");
-	}
-
-	static User createIan() {
-		return createFullUSer("Ian Murdock", "debian", "ian@debian.org", "mot de passe");
-	}
+    static User createIan() {
+        return createFullUSer("Ian Murdock", "debian", "ian@debian.org", "mot de passe");
+    }
 }
